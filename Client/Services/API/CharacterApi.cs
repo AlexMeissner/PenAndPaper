23acf1037af03699501be332aad43477d2a37806
Alexander Meissner <alexander.meissner@rwth-aachen.de>
2023-09-17 10:22:48 +0200

    character creation client service

--- Client/Services/API/CharacterApi.cs
+++ Client/Services/API/CharacterApi.cs
@@ -6,22 +6,36 @@ namespace Client.Services.API
 {
     public interface ICharacterApi
     {
+        public Task<HttpResponse<CharacterCreationDto>> GetAsync(int characterId);
         public Task<HttpResponse<CharacterOverviewDto>> GetOverviewAsync(int campaignId);
+        public Task<HttpResponse> PostAsync(CharacterCreationDto payload);
     }
 
     [TransistentService]
     public class CharacterApi : ICharacterApi
     {
-        private readonly HttpRequest _request;
+        private readonly HttpRequest _characterRequest;
+        private readonly HttpRequest _overviewRequest;
 
         public CharacterApi(IEndPointProvider endPointProvider)
         {
-            _request = new(endPointProvider.BaseURL + "CharacterOverview");
+            _characterRequest = new(endPointProvider.BaseURL + "Character");
+            _overviewRequest = new(endPointProvider.BaseURL + "CharacterOverview");
+        }
+
+        public Task<HttpResponse<CharacterCreationDto>> GetAsync(int characterId)
+        {
+            return _characterRequest.GetAsync<CharacterCreationDto>($"characterId={characterId}");
         }
 
         public Task<HttpResponse<CharacterOverviewDto>> GetOverviewAsync(int campaignId)
         {
-            return _request.GetAsync<CharacterOverviewDto>($"campaignId={campaignId}");
+            return _overviewRequest.GetAsync<CharacterOverviewDto>($"campaignId={campaignId}");
+        }
+
+        public Task<HttpResponse> PostAsync(CharacterCreationDto payload)
+        {
+            return _characterRequest.PostAsync(payload);
         }
     }
 }
