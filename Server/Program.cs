646b02ed13ede133b01c2dc8e5a2bf1a8fb96d0c
Alexander Meissner <alexander.meissner@rwth-aachen.de>
2024-04-13 19:39:19 +0200

    improve logging

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,38 +1,59 @@
-using Server.Middleware;
+using Serilog;
+using Serilog.Events;
 using Server.Services;
 
-class ServerMain
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+
+try
 {
-    public static void Main(string[] args)
+    var builder = WebApplication.CreateBuilder(args);
+
+    Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .CreateLogger();
+
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext());
+
+    builder.Services.AddDatabase(builder.Configuration);
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddSingletonServices();
+    builder.Services.AddScopedServices();
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    // SWAGGER: Automatically open Browser -> Server > Properties > Debug > Open Debug launch profile UI > Launch browser
+    // https://localhost:7099/swagger/index.html
+    if (app.Environment.IsDevelopment())
     {
-        var builder = WebApplication.CreateBuilder(args);
-        builder.CreateLogger();
-        // builder.Configuration.AddJsonFile("appsettings.json"); // It seems like this is done automatically
-
-        builder.Services.AddDatabase(builder.Configuration);
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-        builder.Services.AddSingletonServices();
-        builder.Services.AddScopedServices();
-
-        var app = builder.Build();
-
-        // SWAGGER: Automatically open Browser -> Server > Properties > Debug > Open Debug launch profile UI > Launch browser
-        // https://localhost:7099/swagger/index.html
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-            app.UseHttpsRedirection();
-        }
-
-        app.UseMiddleware<HttpLogger>();
-        app.UseWebSockets();
-        app.UseAuthorization();
-        app.MapControllers();
-        app.MigrateDatabase();
-        app.UpdateRulesInDatabase();
-        app.Run();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseHttpsRedirection();
     }
+
+    app.UseWebSockets();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.MigrateDatabase();
+    app.UpdateRulesInDatabase();
+    app.Run();
+}
+catch (Exception exception)
+{
+    Log.Fatal(exception, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
