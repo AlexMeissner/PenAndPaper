369ce884dc1387f8a735475ebab6ff6f5a6ebb41
Alexander Meissner <alexander.meissner@rwth-aachen.de>
2024-08-24 10:15:36 +0200

    map popup component

--- DataTransfer/Map/MapOverviewDto.cs
+++ DataTransfer/Map/MapOverviewDto.cs
@@ -1,6 +1,9 @@
 namespace DataTransfer.Map
 {
-    public record MapOverviewItemDto(string Name, int MapId, byte[] ImageData);
+    public record MapOverviewItemDto(string Name, int MapId, byte[] ImageData)
+    {
+        public string ImageBase64 => Convert.ToBase64String(ImageData);
+    }
 
     public record MapOverviewDto(ICollection<MapOverviewItemDto> Items);
 }
