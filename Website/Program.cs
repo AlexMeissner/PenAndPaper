1b1bafe8b8c4565f18e0728d67eb7cc07e3c06bb
Alexander Meissner <alexander.meissner@rwth-aachen.de>
2024-11-23 11:40:18 +0100

    add serilog to website project

--- Website/Program.cs
+++ Website/Program.cs
@@ -1,67 +1,92 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Authentication.OAuth;
+using Serilog;
+using Serilog.Events;
 using System.Security.Claims;
 using System.Text.Json;
 using Website.Components;
 using Website.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-builder.Services.AddHttpContextAccessor();
-builder.Services.RegisterServicesFromAttributes();
-
-builder.Services.AddAuthentication(options =>
+try
 {
-    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-})
-    .AddCookie()
-    .AddGoogle(configureOptions =>
+    var builder = WebApplication.CreateBuilder(args);
+
+    Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Configuration)
+            .CreateLogger();
+    builder.Host.UseSerilog(Log.Logger);
+
+    builder.Services.AddRazorComponents()
+        .AddInteractiveServerComponents();
+
+    builder.Services.AddHttpContextAccessor();
+    builder.Services.RegisterServicesFromAttributes();
+
+    builder.Services.AddAuthentication(options =>
     {
-        configureOptions.ClientId = builder.Configuration["Google:ClientId"] ?? throw new Exception("ClientId not found");
-        configureOptions.ClientSecret = builder.Configuration["Google:ClientSecret"] ?? throw new Exception("ClientSecret not found");
-        configureOptions.SaveTokens = true;
-        configureOptions.Scope.Add("openid");
-        configureOptions.Scope.Add("profile");
-        configureOptions.Scope.Add("email");
-        configureOptions.Events = new OAuthEvents
+        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+    })
+        .AddCookie()
+        .AddGoogle(configureOptions =>
         {
-            OnCreatingTicket = context =>
+            configureOptions.ClientId = builder.Configuration["Google:ClientId"] ?? throw new Exception("ClientId not found");
+            configureOptions.ClientSecret = builder.Configuration["Google:ClientSecret"] ?? throw new Exception("ClientSecret not found");
+            configureOptions.SaveTokens = true;
+            configureOptions.Scope.Add("openid");
+            configureOptions.Scope.Add("profile");
+            configureOptions.Scope.Add("email");
+            configureOptions.Events = new OAuthEvents
             {
-                if (context.TokenResponse.Response is JsonDocument response)
+                OnCreatingTicket = context =>
                 {
-                    var idToken = response.RootElement.GetProperty("id_token").ToString();
-                    context.Identity?.AddClaim(new Claim("id_token", idToken));
+                    if (context.TokenResponse.Response is JsonDocument response)
+                    {
+                        var idToken = response.RootElement.GetProperty("id_token").ToString();
+                        context.Identity?.AddClaim(new Claim("id_token", idToken));
+                    }
+
+                    return Task.CompletedTask;
                 }
+            };
+        });
+    builder.Services.AddAuthorization(configure => configure.FallbackPolicy = configure.DefaultPolicy);
 
-                return Task.CompletedTask;
-            }
-        };
-    });
-builder.Services.AddAuthorization(configure => configure.FallbackPolicy = configure.DefaultPolicy);
+    var app = builder.Build();
 
-var app = builder.Build();
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHttpsRedirection();
+    app.MapStaticAssets();
+    app.UseSerilogRequestLogging();
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.UseAntiforgery();
+
+    app.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.MapStaticAssets();
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
+catch (Exception exception)
+{
+    Log.Fatal(exception, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
