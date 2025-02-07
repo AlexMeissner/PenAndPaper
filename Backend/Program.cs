bf0666e0ca4764f624ac5a5dec9b22e273861dc5
Alexander Meissner <alexander.meissner@rwth-aachen.de>
2025-02-07 21:34:46 +0100

    create webapplication extensions

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,36 +1,23 @@
-using Backend.Database;
-using Microsoft.EntityFrameworkCore;
+using Backend;
 
 var builder = WebApplication.CreateBuilder(args);
 
-{
-    var connectionString = builder.Configuration.GetConnectionString("PostgresDb");
-    builder.Services.AddDbContext<PenAndPaperDatabase>(options => options.UseNpgsql(connectionString));
-}
+builder.AddDatabase();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-{
-    using var scope = app.Services.CreateScope();
-    var services = scope.ServiceProvider;
-    var dbContext = services.GetRequiredService<PenAndPaperDatabase>();
-    dbContext.Database.Migrate();
-}
+app.MigrateDatabase();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
