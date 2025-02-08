448efa399ba70b2556422eda593cefc55c01735c
Alexander Meissner <alexander.meissner@rwth-aachen.de>
2025-02-08 09:59:52 +0100

    add serilog

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,23 +1,66 @@
 using Backend;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.AddDatabase();
-
-builder.Services.AddControllers();
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-app.MigrateDatabase();
-
-if (app.Environment.IsDevelopment())
+AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
 {
-    app.MapOpenApi();
+    try
+    {
+        Log.Fatal((Exception)e.ExceptionObject, "Unhandled exception");
+    }
+    catch (Exception exception)
+    {
+        Log.Fatal(exception, "Could not retrieve information from unhandled exception");
+    }
+
+    Log.CloseAndFlush();
+};
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.AddServiceDefaults();
+
+    Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .WriteToAspire(builder.Configuration)
+        .CreateLogger();
+    builder.Host.UseSerilog(Log.Logger);
+
+    builder.AddDatabase();
+
+    builder.Services.AddControllers();
+    builder.Services.AddOpenApi();
+    builder.Services.AddSignalR();
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+    app.MigrateDatabase();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-
-app.Run();
+catch (Exception exception)
+{
+    Log.Fatal(exception, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
