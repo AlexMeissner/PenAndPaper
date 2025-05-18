f87ccef29129f57b68d6d2d52b97c59354a99935
Alexander Meissner <alexander.meissner@rwth-aachen.de>
2025-05-18 16:05:22 +0200

    send  mapupdatedto instead of scriptupdate dto as it is expected from the endpoint

--- ApiClient/ScriptApi.cs
+++ ApiClient/ScriptApi.cs
@@ -22,9 +22,10 @@ public class ScriptApi(IRequestBuilder requestBuilder) : IScriptApi
     {
         return requestBuilder.Path("campaigns", campaignId, "scripts").GetAsync<IEnumerable<ScriptsDto>>();
     }
-    
+
     public Task<Response> Update(int mapId, ScriptUpdateDto payload)
     {
-        return requestBuilder.Path("maps", mapId).PatchAsync(payload);
+        var mapUpdate = new MapUpdateDto(null, payload, null);
+        return requestBuilder.Path("maps", mapId).PatchAsync(mapUpdate);
     }
 }
